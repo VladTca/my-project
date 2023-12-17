25865367bc412589f01506ff9f02446f52d1c61e
VladTca <votsats@gmail.com>
2023-12-17 18:33:11 +0100

    fix

--- jav1.js
+++ jav1.js
@@ -2,19 +2,4 @@ console.log("Дорогу осилит идущий");
 //test
 // let  prod = Number ( prompt () );
 //console.log(prod+5);
-const readline = require('readline').createInterface({
-
-    input: process.stdin,
-
-    output: process.stdout
-
-});
-
-readline.question('Как вас зовут?', name => {
-
-    console.log(name);
-
-    readline.close();
-
-});
 // let prod = prompt ('mnf');
