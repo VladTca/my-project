ab7937d8ef370de05f72a66d342bfbe8f03d9b3a
VladTca <votsats@gmail.com>
2023-12-17 09:17:19 +0100

    test comment 2 tred

--- javs1.js
+++ javs1.js
@@ -1,2 +1,19 @@
 console.log("Дорогу осилит идущий");
 console.log("Дорогу осилит идущий");
+console.log("gopka");
+// sovsem gopka
+// console.log(“gg”);
+console.log('gopkkkka, uz'); // test
+console.log("Столица Гондураса - Тегусигальпа");
+/* console.log("Столица Никарагуа - Манагуа");
+console.log("Столица Коста-Рики - Сан-Хосе"); */
+console.log('JavaSript');
+// coment onestring
+console.log('Погружение');
+/* why this
+tred gi
+ */
+console.log('Java');
+console.log('Script');
+/* comment
+comment  */
